04d94bec708a969671750b1eb536acb0be89b2b0
Karina <vazquez.h.karina@gmail.com>
2022-04-09 22:20:19 -0500

    Consumo de Api mercado libre

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const url = 'https://api.mercadolibre.com/sites/MLA/search?category=MLA1051&sort=price_asc&nano&limit=50&search_type=scan'
+  const [todos, setTodos] = useState()
+
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+    setTodos(responseJSON.results)
+  }
+
+  useEffect(() => {
+    fetchApi()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {
+      !todos ? 'Cargando ...' : todos.map((todo, index)=>{
+        return <li key={todo.id}> {todo.title}</li>
+      })
+    }
     </div>
   );
 }
